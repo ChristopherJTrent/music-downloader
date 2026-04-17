f5adbbb631c3f2296bbbed41b22f8bbf4f3e1c9c
Christopher Trent <ChristopherJTrent@outlook.com>
2026-04-17 12:51:56 -0700

    fix JPA errors in RequestedAlbum

--- src/main/java/net/arkevorkhat/music_downloader/entity/RequestedAlbum.java
+++ src/main/java/net/arkevorkhat/music_downloader/entity/RequestedAlbum.java
@@ -9,37 +9,37 @@ import jakarta.persistence.Id;
 @Entity
 public class RequestedAlbum {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column(unique = true)
-	private String Url;
-	private String Artist;
-	private String Album;
+	private String url;
+	private String artist;
+	private String album;
 	public RequestedAlbum() {}
 	
 	public RequestedAlbum(String uRL, String artist, String album) {
-		Url = uRL;
-		Artist = artist;
-		Album = album;
+		url = uRL;
+		this.artist = artist;
+		this.album = album;
 	}
 
 	public String getUrl() {
-		return Url;
+		return url;
 	}
 	public void setUrl(String uRL) {
-		Url = uRL;
+		url = uRL;
 	}
 	public String getArtist() {
-		return Artist;
+		return artist;
 	}
 	public void setArtist(String artist) {
-		Artist = artist;
+		this.artist = artist;
 	}
 	public String getAlbum() {
-		return Album;
+		return album;
 	}
 	public void setAlbum(String album) {
-		Album = album;
+		this.album = album;
 	}
 
 }
--- src/main/java/net/arkevorkhat/music_downloader/repository/RequestedAlbumRepository.java
+++ src/main/java/net/arkevorkhat/music_downloader/repository/RequestedAlbumRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import net.arkevorkhat.music_downloader.entity.RequestedAlbum;
 
 public interface RequestedAlbumRepository extends CrudRepository<RequestedAlbum, Long> {
-	RequestedAlbum findByUrl(String URL);
+	RequestedAlbum findByUrl(String Url);
 }
